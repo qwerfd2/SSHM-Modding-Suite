3f6756cc0d20f62a763481f62fcf832ef8b4cf9f
Tony Tong <52387376+qwerfd2@users.noreply.github.com>
2023-03-21 12:51:29 -0500

    Improvements thanks to ChatGPT
    insane.

--- script.py
+++ script.py
@@ -4,62 +4,22 @@ import os
 
 print("Opening the file.")
 with open(sys.argv[1], 'rb') as f:
+    print("File opened.")
+    print("Detecting number of files.")
+    file_num = int(f.readline().decode().strip())
+    print(str(file_num),"file(s) detected.")
+    file_list = []
+    for i in range(file_num):
+        file_info = f.readline().decode().strip().split('|')
+        path, start, length = file_info
+        file_object = {'name': path, 'start': int(start), 'end': int(length)}
+        file_list.append(file_object)
+    print("All file metadata acquired.")
     contents = f.read()
 bytes = list(contents)
-print("File opened.")
 
-#Step 1: get the file count (every byte till 1st 0A)
-print("Detecting number of files.")
-file_num = 0
 temp_storage = ""
 temp_index = 0
-for byte in bytes:
-    temp_index += 1
-    temp_storage += chr(byte)
-    if byte == 10:
-        file_num = int(temp_storage)
-        break
-
-bytes = bytes[temp_index:]
-print(str(file_num),"file(s) detected.")
-temp_storage = ""
-temp_index = 0
-
-#Step 2: parse file name, start, length until file runs out
-print("Acquiring file metadata.")
-file_list = []
-file_object = {}
-index = 0
-deletes = 0
-for byte in bytes:
-    deletes += 1
-    if byte == 124:
-        #|
-        if temp_index == 0:
-            file_object["name"] = temp_storage
-            temp_storage = ""
-            temp_index += 1
-        elif temp_index == 1:
-            file_object["start"] = int(temp_storage)
-            temp_storage = ""
-            temp_index += 1  
-        else:
-            raise Exception('file element overflow - more element than expected, aborting.')
-    elif byte == 10:
-        #/n
-        file_object["end"] = int(temp_storage)
-        temp_storage = ""
-        file_list.append(file_object)
-        file_object = {}
-        index += 1
-        temp_index = 0
-        if index >= file_num:
-            break
-    else:
-        temp_storage += chr(byte)
-
-print("All file metadata acquired.")
-bytes = bytes[deletes:]
 
 #step 3: correct offset and extract
 print("Extracting file(s) to 'output' directory.")
