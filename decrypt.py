a68620595c37123489b5d502cae90e306cb612e2
Tony Tong <52387376+qwerfd2@users.noreply.github.com>
2023-04-16 07:59:04 -0500

    Updated to bytearray impl

--- decrypt.py
+++ decrypt.py
@@ -1,13 +1,15 @@
 import os
 
-char_map = [{"as": "a", "bs": "p"}, {"as": "A", "bs": "P"}, {"as": "1", "bs": " "}, {"as": "b", "bs": "s"}, {"as": "B", "bs": "S"}, {"as": "2", "bs": "#"},
-            {"as": "c", "bs": "r"}, {"as": "C", "bs": "R"}, {"as": "3", "bs": "\""}, {"as": "d", "bs": "u"}, {"as": "D", "bs": "U"}, {"as": "4", "bs": "%"},
-            {"as": "e", "bs": "t"}, {"as": "E", "bs": "T"}, {"as": "5", "bs": "$"}, {"as": "f", "bs": "w"}, {"as": "F", "bs": "W"}, {"as": "6", "bs": "'"},
-            {"as": "g", "bs": "v"}, {"as": "G", "bs": "V"}, {"as": "7", "bs": "&"}, {"as": "h", "bs": "y"}, {"as": "H", "bs": "Y"}, {"as": "8", "bs": ")"},
-            {"as": "i", "bs": "x"}, {"as": "I", "bs": "X"}, {"as": "9", "bs": "("}, {"as": "j", "bs": "{"}, {"as": "J", "bs": "["}, {"as": "0", "bs": "!"},
-            {"as": "k", "bs": "z"}, {"as": "K", "bs": "Z"}, {"as": ",", "bs": "="}, {"as": "l", "bs": "}"}, {"as": "L", "bs": "]"}, {"as": "-", "bs": "<"},
-            {"as": "m", "bs": "|"}, {"as": "M", "bs": "\\"},{"as": ":", "bs": "+"}, {"as": "n", "bs": "\x7f"}, {"as": "N", "bs": "_"}, {"as": ".", "bs": "?"}, 
-            {"as": "o", "bs": "~"}, {"as": "O", "bs": "^"}, {"as": "/", "bs": ">"}, {"as": "q", "bs": "`"}, {"as": "Q", "bs": "@"}]
+char_map = [{"as": "a", "bs": "p"}, {"as": "A", "bs": "P"}, {"as": "b", "bs": "s"}, {"as": "B", "bs": "S"}, {"as": "1", "bs": " "},
+            {"as": "c", "bs": "r"}, {"as": "C", "bs": "R"}, {"as": "d", "bs": "u"}, {"as": "D", "bs": "U"}, {"as": "2", "bs": "#"},
+            {"as": "e", "bs": "t"}, {"as": "E", "bs": "T"}, {"as": "f", "bs": "w"}, {"as": "F", "bs": "W"}, {"as": "3", "bs": "\""},
+            {"as": "g", "bs": "v"}, {"as": "G", "bs": "V"}, {"as": "h", "bs": "y"}, {"as": "H", "bs": "Y"}, {"as": "4", "bs": "%"},
+            {"as": "i", "bs": "x"}, {"as": "I", "bs": "X"}, {"as": "j", "bs": "{"}, {"as": "J", "bs": "["}, {"as": "5", "bs": "$"},
+            {"as": "k", "bs": "z"}, {"as": "K", "bs": "Z"}, {"as": "l", "bs": "}"}, {"as": "L", "bs": "]"}, {"as": "6", "bs": "'"},
+            {"as": "m", "bs": "|"}, {"as": "M", "bs": "\\"}, {"as": "n", "bs": "\x7f"}, {"as": "N", "bs": "_"}, {"as": "7", "bs": "&"},
+            {"as": "o", "bs": "~"}, {"as": "O", "bs": "^"}, {"as": "q", "bs": "`"}, {"as": "Q", "bs": "@"}{"as": "8", "bs": ")"},
+            {"as": "9", "bs": "("}, {"as": "0", "bs": "!"}, {"as": ",", "bs": "="}, {"as": "-", "bs": "<"}, {"as": ":", "bs": "+"},
+            {"as": ".", "bs": "?"},  {"as": "/", "bs": ">"}]
 
 folder_path = os.path.dirname(os.path.abspath(__file__))
 save_file = "player.prf"
@@ -24,18 +26,20 @@ for mapping in char_map:
     char_dict[mapping["as"]] = mapping["bs"]
     char_dict[mapping["bs"]] = mapping["as"]
 
-with open(save_file, "r") as input_file:
+with open(save_file, "rb") as input_file:
     text = input_file.read()
 
-new_text = ""
-for char in text:
-    new_char = char_dict.get(char, char)
-    if new_char == char:
-        print("Warning: Undocumented character", char, ", left as-is.")
-    new_text += new_char
+output_data = bytearray()
+for byte in text:
+    char = chr(byte)
+    if char in char_dict:
+        output_data.append(ord(char_dict[char]))
+    else:
+        output_data.append(byte)
+        print("Warning: Undocumented character", byte, ", left as-is.")
 
-print("Decryption complete. Writing file...")
 with open("plaintext.prf", "w") as output_file:
-    output_file.write(new_text)
+    print("Decryption complete. Writing file...")
+    output_file.write(output_data.decode("latin-1"))
 
 print("'plaintext.prf' saved to the same directory.")
